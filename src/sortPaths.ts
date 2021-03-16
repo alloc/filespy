205f89de73fe86793f31d2dfdda6b1b7e7bdad0d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-16 14:41:07 -0400

    fix: ensure children follow immediate parent

--- src/sortPaths.ts
+++ src/sortPaths.ts
@@ -1,15 +1,13 @@
-// https://github.com/hughsk/path-sort
+// Adapted from https://github.com/hughsk/path-sort
 export function sortPaths(as: string, bs: string) {
   let a = as.split('/')
   let b = bs.split('/')
-  let l = Math.max(a.length, b.length)
+  let l = Math.min(a.length, b.length)
   for (let i = 0; i < l; i += 1) {
-    if (!(i in a)) return -1
-    if (!(i in b)) return +1
-    if (a[i].toUpperCase() > b[i].toUpperCase()) return +1
-    if (a[i].toUpperCase() < b[i].toUpperCase()) return -1
-    if (a.length < b.length) return -1
-    if (a.length > b.length) return +1
+    let ac = a[i].toUpperCase()
+    let bc = b[i].toUpperCase()
+    if (ac > bc) return +1
+    if (ac < bc) return -1
   }
-  return 0
+  return a.length - b.length
 }
